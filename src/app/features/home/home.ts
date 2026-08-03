1bc0c0a0b1ff4c5cc5f3326176835a488330969f
Benjamin Rodriguez <devbeex@gmail.com>
2026-08-03 00:21:23 -0500

    chore: set up initial project structure

---
+++ src/app/features/home/home.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  imports: [],
+  templateUrl: './home.html',
+})
+export class Home {
+  
+}
---
+++ src/app/shared/models/project.model.ts
@@ -0,0 +1,9 @@
+export interface Project {
+    title: string;
+    description: string;
+    image: string;
+    technologies: string[];
+    repositoryUrl?: string;
+    demoUrl?: string;
+    featured: boolean;
+}
